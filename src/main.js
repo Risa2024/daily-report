b07016128d39573208077c8f144f9836e00f2573
Risa Goto <omusubi5656dup@gmail.com>
2024-12-31 02:20:37 +0900

    fix:調整

--- src/main.js
+++ src/main.js
@@ -1,1 +1,60 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAGe10Tolg4iVwQQijiweU9LO2nPT97Sp4",
+  authDomain: "daily-report-90f74.firebaseapp.com",
+  projectId: "daily-report-90f74",
+  storageBucket: "daily-report-90f74.firebasestorage.app",
+  messagingSenderId: "709625491912",
+  appId: "1:709625491912:web:baa203917a1eefe65290fd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Cloud Firestoreの初期化
+const db = getFirestore(app);
+
+// Cloud Firestoreから取得したデータを表示する
+const fetchHistoryData = async () => {
+  let tags = "";
+
+  // reportsコレクションのデータを取得
+  const querySnapshot = await getDocs(collection(db, "reports"));
+  
+  // データをテーブル表の形式に合わせてHTMLに挿入
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+    tags += `<tr><td>${doc.data().date}</td><td>${doc.data().name}</td><td>${doc.data().work}</td><td>${doc.data().comment}</td></tr>`;
+  });
+  document.getElementById("js-history").innerHTML = tags;
+};
+
+// Cloud Firestoreから取得したデータを表示する
+if (document.getElementById("js-history")) {
+  fetchHistoryData(getDocs, collection, db);
+}
+
+// Cloud Firestoreにデータを送信する
+const submitData = async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  
+  try {
+    const docRef = await addDoc(collection(db, "reports"), {
+      date: new Date(),
+      name: formData.get("name"),
+      work: formData.get("work"),
+      comment: formData.get("comment"),
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
+
+// Cloud Firestoreにデータを送信する
+if (document.getElementById("js-form")) {
+  document.getElementById("js-form").addEventListener("submit", (e) => submitData(e, addDoc, collection, db));
+}
