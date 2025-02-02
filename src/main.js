e3562d5eea6e39e284678ca67d1896f554f89e12
Risa Goto <omusubi5656dup@gmail.com>
2025-02-02 22:13:24 +0900

    fix:重複箇所

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { fetchHistoryData } from "./my-modules/fetchHistoryData.js";
+import { submitData } from "./my-modules/submit-data.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,7 +12,7 @@ const firebaseConfig = {
   projectId: "daily-report-7eff0",
   storageBucket: "daily-report-7eff0.firebasestorage.app",
   messagingSenderId: "658851324236",
-  appId: "1:658851324236:web:735906b84f6c42cc192fe9"
+  appId: "1:658851324236:web:735906b84f6c42cc192fe9",
 };
 
 // Initialize Firebase
@@ -21,45 +21,14 @@ const app = initializeApp(firebaseConfig);
 // Cloud Firestoreの初期化
 const db = getFirestore(app);
 
-// Cloud Firestoreから取得したデータを表示する
-const fetchHistoryData = async () => {
-  let tags = "";
-
-  // reportsコレクションのデータを取得
-  const querySnapshot = await getDocs(collection(db, "reports"));
-  
-  // データをテーブル表の形式に合わせてHTMLに挿入
-  querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data()}`);
-    tags += `<tr><td>${doc.data().date}</td><td>${doc.data().name}</td><td>${doc.data().work}</td><td>${doc.data().comment}</td></tr>`;
-  });
-  document.getElementById("js-history").innerHTML = tags;
-};
-
-// Cloud Firestoreから取得したデータを表示する
-if (document.getElementById("js-history")) {
-  fetchHistoryData(getDocs, collection, db);
-}
-
-// Cloud Firestoreにデータを送信する
-const submitData = async (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  
-  try {
-    const docRef = await addDoc(collection(db, "reports"), {
-      date: new Date(),
-      name: formData.get("name"),
-      work: formData.get("work"),
-      comment: formData.get("comment"),
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
+  //fetchHistoryDataインポート
+  if (document.getElementById("js-history")) {
+    fetchHistoryData(getDocs, collection, db);
   }
-};
 
-// Cloud Firestoreにデータを送信する
-if (document.getElementById("js-form")) {
-  document.getElementById("js-form").addEventListener("submit", (e) => submitData(e, addDoc, collection, db));
-}
+  //submitDataインポート
+  if (document.getElementById("js-form")) {
+    document
+      .getElementById("js-form")
+      .addEventListener("submit", (e) => submitData(e, addDoc, collection, db));
+  }
